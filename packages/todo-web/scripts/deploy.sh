ba8f8d2e290ff37bb68d6dfce68e72e598091e19
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-08-24 14:19:26 -0400

    Deploy

--- packages/todo-web/scripts/deploy.sh
+++ packages/todo-web/scripts/deploy.sh
@@ -4,7 +4,7 @@ git commit -m "Deploy"
 git push
 scp client/.env.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/todos/client/.env.production
 ssh ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com '
-  cd code/taylors-tech/packages/todos &&
+  cd code/taylors-tech/packages/todo-web &&
   git pull &&
   npm run build &&
   pm2 delete todos &&
