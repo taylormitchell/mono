0e570b4e0bba97a336e2df5637673b8df929a7c1
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-08-24 14:38:57 -0400

    Deploy

--- packages/todo-web/server/dist/index.js
+++ packages/todo-web/server/dist/index.js
@@ -7,16 +7,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
-const todo_1 = require("./todo");
+const todo_1 = require("@taylor/common/todo");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-const rootDir = path_1.default.join(__dirname, "../../../notes");
 // Middleware
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 // Mock API route
 app.get("/api/data", (req, res) => {
-    const todos = (0, todo_1.getTodos)(rootDir);
+    const todos = (0, todo_1.getTodos)();
     if (todos.length === 0) {
         res.json({ todos: [] });
     }
@@ -25,20 +24,19 @@ app.get("/api/data", (req, res) => {
     }
 });
 // Update todo endpoint
-app.put("/api/todo", (req, res) => {
-    const updatedTodo = req.body;
-    if (updatedTodo.due) {
-        updatedTodo.due = new Date(updatedTodo.due);
-    }
-    // Attempt to update the todo in the file
-    const updated = (0, todo_1.updateTodo)(updatedTodo);
-    if (updated) {
-        res.json({ todo: updatedTodo });
-    }
-    else {
-        res.status(500).json({ error: "Failed to update todo" });
-    }
-});
+// app.put("/api/todo", (req, res) => {
+//   const updatedTodo = req.body as Todo;
+//   if (updatedTodo.due) {
+//     updatedTodo.due = new Date(updatedTodo.due);
+//   }
+//   // Attempt to update the todo in the file
+//   const updated = updateTodo(updatedTodo);
+//   if (updated) {
+//     res.json({ todo: updatedTodo });
+//   } else {
+//     res.status(500).json({ error: "Failed to update todo" });
+//   }
+// });
 // Serve static files in production
 if (process.env.NODE_ENV === "production") {
     const frontendBuildPath = path_1.default.join(__dirname, "../../client/dist");
