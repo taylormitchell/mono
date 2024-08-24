2b446e2e571ee2135b3df4ee1a7be6f8778ad16b
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-08-24 15:19:41 -0400

    Add sw

--- packages/todo-web/client/vite.config.ts
+++ packages/todo-web/client/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Todo Web App",
+        short_name: "Todo",
+        start_url: "/",
+        display: "standalone",
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": "http://localhost:3000",
