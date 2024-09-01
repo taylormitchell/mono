f9c159b9a31735e19a222f22d0581cf1459940c6
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-01 12:05:54 -0400

    save

--- packages/note-cli/scripts/deploy-create-and-commit-todays-note.sh
+++ packages/note-cli/scripts/deploy-create-and-commit-todays-note.sh
@@ -1,5 +1,5 @@
 # Assumes you're running this from the root of note-cli package
-job="0 0 * * * bash $PWD/scripts/create-and-commit-todays-note.sh"
+job="* * * * * bash $PWD/scripts/create-and-commit-todays-note.sh"
 
 # Check if the job already exists in the crontab
 if ! crontab -l | grep -Fq "create-and-commit-todays-note"; then
