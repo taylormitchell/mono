b59695684d5ccb9654cbd2c2124d436f324c4489
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-01 12:03:36 -0400

    save

---
+++ packages/note-cli/scripts/deploy-create-and-commit-todays-note.sh.sh
@@ -0,0 +1,11 @@
+# Assumes you're running this from the root of note-cli package
+job="0 0 * * * bash scripts/create-and-commit-todays-note.sh"
+
+# Check if the job already exists in the crontab
+if ! crontab -l | grep -Fq "$job"; then
+    # If the job doesn't exist, add it to the crontab
+    (crontab -l 2>/dev/null; echo "$job") | crontab -
+    echo "Cron job added successfully."
+else
+    echo "Cron job already exists. No changes made."
+fi
