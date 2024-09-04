f94389c5cd18f45b95c1add73f7baa3ab0febbb2
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-04 09:00:01 -0400

    save

--- packages/common/todo/parsers.ts
+++ packages/common/todo/parsers.ts
@@ -243,11 +243,6 @@ export function listAllTodos(path?: string): void {
 function lessThanOrEqualTo(date1: Date, date2: Date): boolean {
   // return date1.toISOString().split("T")[0] <= date2.toISOString().split("T")[0];
   // same logic but comparing the year, month, and day
-  // return (
-  //   date1.getFullYear() < date2.getFullYear() ||
-  //   (date1.getFullYear() === date2.getFullYear() && date1.getMonth() < date2.getMonth()) ||
-  //   (date1.getMonth() === date2.getMonth() && date1.getDate() <= date2.getDate())
-  // );
   if (date1.getFullYear() < date2.getFullYear()) {
     return true;
   } else if (date1.getFullYear() === date2.getFullYear() && date1.getMonth() < date2.getMonth()) {
