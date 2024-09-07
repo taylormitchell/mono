e48deb7e66ca02a9f94c9ab55fb3ab9d6584ea64
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-07 17:28:37 -0400

    save

--- packages/api/scripts/deploy.sh
+++ packages/api/scripts/deploy.sh
@@ -2,11 +2,11 @@
 git add --all
 git commit -m "Deploy"
 git push
-scp client/.env.remote.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/todo-web/client/.env.production
+scp .env ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/api/.env
 ssh ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com '
-  cd code/taylors-tech/packages/todo-web &&
+  cd code/taylors-tech/packages/api &&
   git pull &&
   npm run build &&
-  pm2 delete todos || true &&
-  pm2 start npm --name todos -- run start
+  pm2 delete api || true &&
+  pm2 start npm --name api -- run start
 '
