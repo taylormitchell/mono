b079d15d96fe8e93fa2fe137d904ca02444995ee
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-08 10:58:34 -0400

    Deploy

--- packages/todo-web/client/scripts/deploy.sh
+++ packages/todo-web/client/scripts/deploy.sh
@@ -2,7 +2,7 @@
 git add --all
 git commit -m "Deploy"
 git push
-scp .env.remote.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/todo-web/client/.env.production
+scp .env.remote.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/todo-web/client/.env
 ssh ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com '
   cd code/taylors-tech/packages/todo-web/client &&
   git pull &&
