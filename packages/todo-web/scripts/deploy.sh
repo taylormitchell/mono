9869a000a46c6eaa7f6647deb937581efa1df680
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-08 11:10:25 -0400

    remove todo-web/server cause we're using api now

--- packages/todo-web/scripts/deploy.sh
+++ packages/todo-web/scripts/deploy.sh
@@ -2,11 +2,11 @@
 git add --all
 git commit -m "Deploy"
 git push
-scp client/.env.remote.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/todo-web/client/.env.production
+scp .env.remote.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/todo-web/client/.env
 ssh ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com '
-  cd code/taylors-tech/packages/todo-web &&
+  cd code/taylors-tech/packages/todo-web/client &&
   git pull &&
-  npm run build &&
+  npm install &&
   pm2 delete todos || true &&
-  pm2 start npm --name todos -- run start
+  pm2 start npm --name todos -- run dev
 '
