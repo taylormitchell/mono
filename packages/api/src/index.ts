95bf2a4b4ef69ebc78b0f60a2116107efb560e39
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-09 20:17:15 -0400

    save

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -54,10 +54,14 @@ const port = process.env.PORT || 3077;
 
 function gitSync() {
   const stash = execSync("git stash -u", { encoding: "utf-8" });
-  execSync("git pull --rebase", { encoding: "utf-8" });
-  execSync("git push", { encoding: "utf-8" });
+  log.info("Stash output:", stash);
+  const pull = execSync("git pull --rebase", { encoding: "utf-8" });
+  log.info("Pull output:", pull);
+  const push = execSync("git push", { encoding: "utf-8" });
+  log.info("Push output:", push);
   if (!stash.includes("No local changes to save")) {
-    execSync("git stash pop", { encoding: "utf-8" });
+    const pop = execSync("git stash pop", { encoding: "utf-8" });
+    log.info("Pop output:", pop);
   }
 }
 
