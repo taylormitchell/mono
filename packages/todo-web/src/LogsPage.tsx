6e7617612057918e0cf1ff887a02581f7db04943
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-09 21:14:41 -0400

    Deploy

--- packages/todo-web/src/LogsPage.tsx
+++ packages/todo-web/src/LogsPage.tsx
@@ -29,7 +29,7 @@ export function LogsPage({ jwt }: { jwt: string | null }) {
       const response = await fetch(`${apiUrl}/api/log`, {
         method: "POST",
         headers: { "Content-Type": "application/json", Authorization: `Bearer ${jwt}` },
-        body: JSON.stringify({ logEntry, datetime: logEntry.datetime.toISOString() }),
+        body: JSON.stringify({ ...logEntry, datetime: logEntry.datetime.toISOString() }),
       });
 
       if (!response.ok) {
