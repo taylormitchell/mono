43b03297dbc9e41ca9fa698a4bd0246a828359ea
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-09-25 21:04:02 -0400

    Deploy

--- packages/kindle-highlights-extension/src/background.ts
+++ packages/kindle-highlights-extension/src/background.ts
@@ -7,44 +7,10 @@ console.log("Kindle Highlights Extractor extension is running!");
 chrome.runtime.onInstalled.addListener(async () => {
   console.log("Extension installed!!!");
   //   chrome.alarms.create("fetchHighlights", { periodInMinutes: 1 / 6 });
-  chrome.alarms.create("checkLoginStatus", { periodInMinutes: 1 / 6 });
+  chrome.alarms.create("checkLoginStatus", { periodInMinutes: 1 });
   //   checkLoginStatus();
   updateBadge();
-  chrome.storage.local.get("token", async (data) => {
-    console.log(data);
-    const response = await fetch(PUT_HIGHLIGHTS_API_URL, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${data.token}`,
-      },
-      body: JSON.stringify({
-        content: JSON.stringify(
-          {
-            highlights: [
-              {
-                asin: "B07VGRYD1M",
-                id: "01HFFZ5Z5Z5Z5Z5Z5Z5Z5Z5Z5",
-                text: "This is a test highlight",
-                location: "100",
-                createdAt: "2024-09-25T12:00:00Z",
-              },
-            ],
-          },
-          null,
-          2
-        ),
-      }),
-    });
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-    } else {
-      console.error("Failed to put highlights");
-    }
-  });
-
-  //   fetchHighlights();
+  getAndPutHighlights();
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
@@ -56,58 +22,93 @@ chrome.alarms.onAlarm.addListener((alarm) => {
   }
 });
 
-async function fetchHighlights() {
-  chrome.cookies.getAll({ domain: "read.amazon.com" }, async (cookies) => {
-    const cookieHeader = cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
+async function getAndPutHighlights() {
+  chrome.storage.local.get("token", async (data) => {
+    if (!data.token) {
+      console.error("No token found");
+      return;
+    }
+    const highlights = await fetchHighlights();
+    await putHighlights(highlights, data.token);
+  });
+}
 
-    const response = await fetch("https://read.amazon.com/notebook", {
-      method: "GET",
-      headers: {
-        Cookie: cookieHeader,
-      },
-      credentials: "include",
-    });
-    const html = await response.text();
-    const books = await fetchFromOffscreenDocument<Book[]>({
-      type: "get-books",
-      data: { html },
-    });
+async function fetchHighlights(): Promise<Annotation[]> {
+  return new Promise((resolve, reject) => {
+    chrome.cookies.getAll({ domain: "read.amazon.com" }, async (cookies) => {
+      const cookieHeader = cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
 
-    const bookAnnotations = (
-      await Promise.all(
-        books.map(async (book) => {
-          console.debug("fetching", book);
-          const response = await fetch(
-            `https://read.amazon.com/notebook?asin=${book.asin}&contentLimitState=&`,
-            {
-              method: "GET",
-              headers: {
-                Cookie: cookieHeader,
-              },
-              credentials: "include",
-            }
-          );
-          const html = await response.text();
-          console.debug("getting annotations for", book.asin);
-          const annotations = await fetchFromOffscreenDocument<Annotation[]>({
-            type: "get-annotations",
-            data: { html },
-          });
-          return annotations.map((annotation) => ({ ...annotation, ...book }));
-        })
-      )
-    )
-      .flat()
-      .sort((a, b) => {
-        if (a.asin < b.asin) return -1;
-        if (a.asin > b.asin) return 1;
-        if (a.id < b.id) return -1;
-        if (a.id > b.id) return 1;
-        return 0;
+      const response = await fetch("https://read.amazon.com/notebook", {
+        method: "GET",
+        headers: {
+          Cookie: cookieHeader,
+        },
+        credentials: "include",
+      });
+      const html = await response.text();
+      const books = await fetchFromOffscreenDocument<Book[]>({
+        type: "get-books",
+        data: { html },
       });
 
-    console.log(bookAnnotations);
+      const bookAnnotations = (
+        await Promise.all(
+          books.map(async (book) => {
+            console.debug("fetching", book);
+            const response = await fetch(
+              `https://read.amazon.com/notebook?asin=${book.asin}&contentLimitState=&`,
+              {
+                method: "GET",
+                headers: {
+                  Cookie: cookieHeader,
+                },
+                credentials: "include",
+              }
+            );
+            const html = await response.text();
+            console.debug("getting annotations for", book.asin);
+            const annotations = await fetchFromOffscreenDocument<Annotation[]>({
+              type: "get-annotations",
+              data: { html },
+            });
+            return annotations.map((annotation) => ({ ...annotation, ...book }));
+          })
+        )
+      )
+        .flat()
+        .sort((a, b) => {
+          if (a.asin < b.asin) return -1;
+          if (a.asin > b.asin) return 1;
+          if (a.id < b.id) return -1;
+          if (a.id > b.id) return 1;
+          return 0;
+        });
+
+      resolve(bookAnnotations);
+    });
   });
+}
+
+async function putHighlights(highlights: Annotation[], token: string) {
+  try {
+    const content = JSON.stringify({ highlights }, null, 2);
+    const response = await fetch(PUT_HIGHLIGHTS_API_URL, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ content }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      console.log("put highlights request succeeded", { responseData: data });
+    } else {
+      throw new Error(`Request not ok: ${response.status} ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("Error putting highlights", { error });
+  }
 }
 
 async function fetchFromOffscreenDocument<T>({
