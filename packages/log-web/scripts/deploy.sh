2fa9e925f050ac070ce7eaa6c9b24d121913e7d5
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-10-08 08:48:08 -0400

    Deploy

--- packages/log-web/scripts/deploy.sh
+++ packages/log-web/scripts/deploy.sh
@@ -2,9 +2,9 @@
 git add --all
 git commit -m "Deploy"
 git push
-scp .env.remote.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/todo-web/.env
+scp .env.remote.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/log-web/.env
 ssh ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com '
-  cd code/taylors-tech/packages/todo-web &&
+  cd code/taylors-tech/packages/log-web &&
   git pull &&
   npm install &&
   pm2 delete log || true &&
