e9c941f1cbdf35ce9b80c4475491895b6286afdf
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-10-24 08:33:03 -0400

    save

--- packages/mobx-linear/src/test.ts
+++ packages/mobx-linear/src/test.ts
@@ -1,4 +1,4 @@
-import { autorun, observable, runInAction, toJS } from "mobx";
+import { action, makeObservable, observable, reaction, runInAction, toJS } from "mobx";
 
 type Update = {
   operation: "update";
@@ -32,35 +32,51 @@ class Store {
   // TODO rather than calling `commit` or something, can mobx autocommit for me after an action completes?
   // I *think* reactions delay running until an entire action completes, so if processing the changes is
   // done instead an action, will that autocommit it?
-  uncommittedChanges: Action[] = observable.array();
+  uncommittedChanges: Action[] = [];
+  // Using an observable number to trigger a reaction b/c if we track the change array,
+  // the reaction will need to modify it too (clear it) which you're not supposed to do
+  // inside reactions.
+  changeCount: number = 0;
 
   constructor() {
-    // makeObservable(this, {
-    //   uncommittedChanges: observable.shallow,
-    // });
-    autorun(() => {
-      console.log(toJS(this.uncommittedChanges.slice().map((v) => toJS(v))));
+    makeObservable(this, {
+      changeCount: observable,
+      loadProject: action,
+      loadIssue: action,
+      createIssue: action,
+      createProject: action,
     });
+    reaction(
+      () => this.changeCount,
+      () => {
+        console.log(toJS(this.uncommittedChanges.slice().map((v) => toJS(v))));
+        this.uncommittedChanges = [];
+      }
+    );
   }
 
   addChange(change: Action) {
     if (this.trackingChanges) {
+      this.changeCount++;
       this.uncommittedChanges.push(change);
     }
   }
 
   loadProject(project: Project) {
+    this.trackingChanges = false;
     let model = this.projects.get(project.id);
     if (model) {
       model.populatePlaceholder(project);
     } else {
-      model = new ProjectModel(this, project);
+      model = new ProjectModel(this, project.id, project);
       this.projects.set(project.id, model);
     }
+    this.trackingChanges = true;
     return model;
   }
 
-  loadIssue(issue: Issue) {
+  loadIssue(issue: IssueData) {
+    this.trackingChanges = false;
     // Get project
     let project: ProjectModel | null = null;
     if (issue.projectId) {
@@ -75,21 +91,50 @@ class Store {
     if (model) {
       model.populatePlaceholder({ ...issue, project });
     } else {
-      model = new IssueModel(this, { ...issue, project });
+      model = new IssueModel(this, issue.id, { ...issue, project });
       this.issues.set(issue.id, model);
     }
-    // Add to project collection
-    // if (project) {
-    //   // should this be done inside the issue model?
-    //   project._issues.add(model);
-    // }
+    this.trackingChanges = true;
     return model;
+  }
+
+  createIssue(id: string, props: Partial<IssueState>) {
+    if (this.issues.has(id)) {
+      throw new Error(`Issue with id ${id} already exists`);
+    }
+    this.trackingChanges = false;
+    const issue = new IssueModel(this, id, props);
+    this.trackingChanges = true;
+    // TODO maybe this should be done inside the constructor?
+    this.issues.set(id, issue);
+    this.addChange({ operation: "create", model: "issue", id, props: serializeState(props) });
+    return issue;
+  }
+
+  createProject(id: string, props: Partial<ProjectState>) {
+    if (this.projects.has(id)) {
+      throw new Error(`Project with id ${id} already exists`);
+    }
+    this.trackingChanges = false;
+    const project = new ProjectModel(this, id, props);
+    this.trackingChanges = true;
+    this.projects.set(id, project);
+    this.addChange({ operation: "create", model: "project", id, props: serializeState(props) });
+    return project;
   }
 }
 
-abstract class Model {
+type ModelName = "issue" | "project";
+
+abstract class BaseModel {
   abstract id: string;
   abstract store: Store;
+  abstract name: ModelName;
+}
+
+type Model = IssueModel | ProjectModel;
+function isModel(value: unknown): value is Model {
+  return value instanceof IssueModel || value instanceof ProjectModel;
 }
 
 type Project = {
@@ -97,123 +142,84 @@ type Project = {
   title: string;
 };
 
-type Issue = {
+type IssueData = {
   id: string;
   projectId: string | null;
   title: string;
 };
 
-class IssueModel implements Model {
+type IssueState = {
+  project: ProjectModel | null;
+  placeholder: boolean;
+};
+
+class IssueModel implements BaseModel {
+  readonly name = "issue";
   store: Store;
-  _project: ProjectModel | null = null;
+  id: string;
   // todo maybe something like this makes it easier to track changes
   // on all props. then I have fancy getters and setters on the class
   // instance which do compound operations like setProjectAndAddToCollection
-  private state: {
-    id: string;
-    project: ProjectModel | null;
-    placeholder: boolean;
-  };
+  // TODO semi private state. better way?
+  _state: IssueState;
 
   constructor(
     store: Store,
-    {
-      id,
-      project,
-      placeholder = false,
-    }: { id: string; project: ProjectModel | null; placeholder?: boolean }
+    id: string,
+    { project = null, placeholder = false }: Partial<IssueState>
   ) {
     this.store = store;
-    this.state = new Proxy(
-      { id, project, placeholder },
-      {
-        set: (target, prop, value) => {
-          store.addChange({
-            operation: "update",
-            model: "issue",
-            id,
-            oldProps: { [prop as string]: target[prop as keyof typeof target] },
-            newProps: { [prop]: value },
-          });
-          target[prop as keyof typeof target] = value;
-          return true;
-        },
-      }
-    );
-    this._project = this.setProjectAndAddToCollection(project);
-  }
-
-  get id() {
-    return this.state.id;
+    this.id = id;
+    this._state = makeTracking({ project, placeholder }, this);
+    moveIssueToProject(this, project);
   }
 
   get placeholder() {
-    return this.state.placeholder;
-  }
-
-  private setProjectAndAddToCollection(project: ProjectModel | null) {
-    if (this._project) {
-      this._project._issues.delete(this);
-    }
-    this._project = project;
-    if (project) {
-      project._issues.add(this);
-    }
-    return project;
+    return this._state.placeholder;
   }
 
   get project() {
-    return this._project;
+    return this._state.project;
   }
 
   set project(project: ProjectModel | null) {
-    this.setProjectAndAddToCollection(project);
-    this.store.addChange({
-      operation: "update",
-      model: "issue",
-      id: this.id,
-      oldProps: { project: this._project },
-      newProps: { project },
-    });
+    moveIssueToProject(this, project);
   }
 
   static createPlaceholder(store: Store, id: string) {
-    return new IssueModel(store, { id, project: null, placeholder: true });
+    return new IssueModel(store, id, { project: null, placeholder: true });
   }
 
-  populatePlaceholder(props: { id: string; project: ProjectModel | null; title: string }) {
-    this.id = props.id;
-    this._project = props.project;
-    this.placeholder = true;
+  populatePlaceholder(props: Partial<IssueState>) {
+    Object.assign(this._state, props);
   }
 }
 
-function setProjectAndAddToCollection(
-  store: Store,
-  issue: IssueModel,
-  project: ProjectModel | null
-) {
-  if (issue._project) {
-    issue._project._issues.delete(issue);
+function moveIssueToProject(issue: IssueModel, project: ProjectModel | null) {
+  if (issue._state.project) {
+    issue._state.project._state.issues.delete(issue);
   }
-  issue._project = project;
+  issue._state.project = project;
   if (project) {
-    project._issues.add(issue);
+    project._state.issues.add(issue);
   }
-  store.addChange({
-    operation: "update",
-    model: "issue",
-    id: issue.id,
-    oldProps: { project: issue._project },
-    newProps: { project },
-  });
-  return { project, issue };
 }
 
-class ProjectModel implements Model {
-  store: Store;
+type ProjectData = {
   id: string;
   title: string;
+};
+
+type ProjectState = {
+  title: string;
+  placeholder: boolean;
+  issues: Set<IssueModel>;
+};
+
+class ProjectModel implements BaseModel {
+  readonly name = "project";
+  store: Store;
+  id: string;
   /**
    * For now, we're using _ prefix for private-by-convention fields. These are still accessible
    * but should only be used internally. Later I'd like to find a clean way to do this while
@@ -223,41 +229,88 @@ class ProjectModel implements Model {
    * it's nice to have this on the model itself, rather than a issuesByProjectId collection
    * can then we know it always exists
    */
-  _issues: Set<IssueModel>;
-  placeholder: boolean;
+  _state: ProjectState;
 
-  constructor(
-    store: Store,
-    { id, title, placeholder = false }: { id: string; title: string; placeholder?: boolean }
-  ) {
+  constructor(store: Store, id: string, { title, placeholder = false }: Partial<ProjectState>) {
     this.store = store;
     this.id = id;
-    this.title = title;
-    this.placeholder = placeholder;
-    this._issues = new Set();
+    this._state = makeTracking(
+      { title: title || "", placeholder, issues: new Set<IssueModel>() },
+      this
+    );
   }
 
-  populatePlaceholder({
-    id,
-    title,
-    placeholder = false,
-  }: {
-    id: string;
-    title: string;
-    placeholder?: boolean;
-  }) {
-    this.id = id;
-    this.title = title;
-    this.placeholder = placeholder;
+  get title() {
+    return this._state.title;
+  }
+
+  set title(value: string) {
+    this._state.title = value;
+  }
+
+  get placeholder() {
+    return this._state.placeholder;
+  }
+
+  set placeholder(value: boolean) {
+    this._state.placeholder = value;
   }
 
   static createPlaceholder(store: Store, id: string) {
-    return new ProjectModel(store, { id, title: "unknown", placeholder: true });
+    return new ProjectModel(store, id, { title: "unknown", placeholder: true });
+  }
+
+  populatePlaceholder(props: Partial<ProjectState>) {
+    Object.assign(this._state, props);
   }
 
   getIssues() {
-    return this._issues.values();
+    return this._state.issues.values();
   }
+
+  addIssue(issue: IssueModel) {
+    moveIssueToProject(issue, this);
+  }
+
+  removeIssue(issue: IssueModel) {
+    moveIssueToProject(issue, null);
+  }
+}
+
+function isKeyOf<T extends Record<string, any>>(prop: unknown, obj: T): prop is keyof T & string {
+  return typeof prop === "string" && prop in obj;
+}
+
+function makeTracking<T extends Record<string, any>>(initialState: T, model: BaseModel): T {
+  return new Proxy(initialState, {
+    set: (target, prop, value) => {
+      if (isKeyOf(prop, target)) {
+        const oldProps = serializeState({ [prop]: target[prop] });
+        const newProps = serializeState({ [prop]: value });
+        model.store.addChange({
+          operation: "update",
+          model: model.name,
+          id: model.id,
+          oldProps,
+          newProps,
+        });
+        (target as any)[prop] = value;
+      }
+      return true;
+    },
+  });
+}
+
+function serializeState(state: Record<string, any>) {
+  const serialized: Record<string, any> = {};
+  for (const key in state) {
+    if (isModel(state[key])) {
+      serialized[`${key}Id`] = state[key].id;
+    } else {
+      serialized[key] = state[key];
+    }
+  }
+  return serialized;
 }
 
 // class RelationModel implements Model {
@@ -269,7 +322,6 @@ class ProjectModel implements Model {
 //   type: RelationType;
 //   updatedAt: Date;
 //   createdAt: Date;
-
 //   sourceIssues: Collection<IssueModel>;
 //   targetIssues: Collection<IssueModel>;
 
@@ -335,20 +387,21 @@ class ProjectModel implements Model {
 function test() {
   const store = new Store();
 
-  store.loadIssue({ id: "1", projectId: "1", title: "Issue 1" });
-  store.loadIssue({ id: "2", projectId: "1", title: "Issue 2" });
-  store.loadProject({ id: "1", title: "Project 1" });
-  store.loadProject({ id: "2", title: "Project 2" });
+  const issue1 = store.loadIssue({ id: "1", projectId: "1", title: "Issue 1" });
+  const issue2 = store.loadIssue({ id: "2", projectId: "1", title: "Issue 2" });
+  const project1 = store.loadProject({ id: "1", title: "Project 1" });
+  const project2 = store.loadProject({ id: "2", title: "Project 2" });
 
   runInAction(() => {
-    const issue1 = store.issues.get("1");
-    if (issue1) {
-      issue1.project = store.projects.get("2") ?? null;
-    }
-    const issue2 = store.issues.get("2");
-    if (issue2) {
-      issue2.project = store.projects.get("1") ?? null;
-    }
+    issue1.project = project2;
+    issue2.project = project2;
+    project1.title = "Project 1 (updated)";
+    project2.removeIssue(issue1);
+  });
+
+  runInAction(() => {
+    store.createIssue("3", { project: project1, placeholder: false });
+    store.createProject("3", { title: "Project 3", placeholder: false });
   });
 }
 
