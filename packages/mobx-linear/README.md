45900e512a4f5a2d86f58a530a8bcf7b187e5cb2
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-10-31 08:39:48 -0400

    save

--- packages/mobx-linear/README.md
+++ packages/mobx-linear/README.md
@@ -1,5 +1,9 @@
 # MobX Linear
 
+TODO see /Code/temp
+
+TODO play around with decorators again
+
 TODO I was imagining have a proxy around the objects. but then there are two proxies, right? the mobx one and mine (is that right?). if that's the case, that feels weird. Is there a way to hook into the mobx proxy?
 - How are proxies with proxies handled?
 - Can decorators on properties really not be used to add additional functionality on get/set?
--- packages/mobx-linear/src/test8.ts
+++ packages/mobx-linear/src/test8.ts
@@ -1,17 +1,42 @@
-import { makeAutoObservable } from "mobx";
+import { autorun, observable, runInAction } from "mobx";
 
-type Issue = {
-  readonly model: "issue";
-  readonly id: string;
-  title: string;
+const ObservableWithLogs = (target: any, context: ClassAccessorDecoratorContext) => {
+  // First apply the observable decorator
+  const observableResult = observable(target, context);
+  if (!observableResult) {
+    throw new Error("Failed to apply observable decorator");
+  }
+
+  // Then wrap it with our logging functionality
+  return {
+    get() {
+      const value = observableResult.get?.call(this);
+      console.log(`Getting ${String(context.name)}:`, value);
+      return value;
+    },
+    set(newValue: any) {
+      const oldValue = observableResult.get?.call(this);
+      console.log(`Setting ${String(context.name)} from:`, oldValue, "to:", newValue);
+      observableResult.set?.call(this, newValue);
+    },
+    init(value: any) {
+      console.log(`Initializing ${String(context.name)}`, { this: this, context });
+      return observableResult.init?.call(this, value);
+    },
+  };
 };
 
-const issue: Issue = {
-  model: "issue",
-  id: "1",
-  title: "test",
-};
+class Test {
+  @ObservableWithLogs accessor title = "test";
+}
 
-makeAutoObservable(issue);
+const test = new Test();
+// makeAutoObservable(test);
 
-console.log(issue.title);
+autorun(() => {
+  console.log(test.title);
+});
+
+runInAction(() => {
+  test.title = "test2";
+});
