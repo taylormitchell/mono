118625e320f9f1b3dedc8a6b13e82e362b436564
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-08 08:59:13 -0500

    Deploy

--- packages/api/scripts/deploy.sh
+++ packages/api/scripts/deploy.sh
@@ -6,10 +6,10 @@ cp .env .env.production
 echo "AUTH_DISABLED=false" >> .env.production
 echo "COMMIT_ON_SAVE=true" >> .env.production
 echo "SYNC_ENABLED=true" >> .env.production
-scp .env.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/api/.env
+scp .env.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/home/packages/api/.env
 rm .env.production
 ssh ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com '
-  cd code/taylors-tech/packages/api &&
+  cd code/home/packages/api &&
   git stash save "Stashing changes during api deploy $(date)" &&
   git pull --rebase &&
   npm i &&
