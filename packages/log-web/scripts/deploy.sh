d4e763f14eda5b995d6842d61e0c87d66400ebaa
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-08 08:58:11 -0500

    save

--- packages/log-web/scripts/deploy.sh
+++ packages/log-web/scripts/deploy.sh
@@ -2,9 +2,9 @@
 git add --all
 git commit -m "Deploy"
 git push
-scp .env.remote.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/taylors-tech/packages/log-web/.env
+scp .env.remote.production ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com:~/code/home/packages/log-web/.env
 ssh ec2-user@ec2-3-92-45-253.compute-1.amazonaws.com '
-  cd code/taylors-tech/packages/log-web &&
+  cd code/home/packages/log-web &&
   git pull &&
   npm install &&
   pm2 delete log || true &&
