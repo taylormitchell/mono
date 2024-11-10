cfc30a8bf735f39a01edc257e276f6ec37cfa25d
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-10 08:21:01 -0500

    save

--- scripts/publish-repo.sh
+++ scripts/publish-repo.sh
@@ -25,7 +25,8 @@ mkdir "$NEW_REPO_PATH"
 rsync -av --exclude="data" --exclude=".git" --exclude=".github" --exclude=".vscode" --exclude="node_modules" ./ "$NEW_REPO_PATH"
 
 # Create data directory and add .gitignore
-mkdir -p "$NEW_REPO_PATH/data" && touch "$NEW_REPO_PATH/data/.gitignore"
+# mkdir -p "$NEW_REPO_PATH/data" && touch "$NEW_REPO_PATH/data/.gitignore"
+mkdir -p "$NEW_REPO_PATH/data"
 echo "# Personal data removed for privacy" > "$NEW_REPO_PATH/data/.gitignore"
 echo "*" >> "$NEW_REPO_PATH/data/.gitignore"
 
