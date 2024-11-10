5a115ea57368af8ba46b73a07679f537d4a7b8b2
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-09 20:59:00 -0500

    save

--- packages/ideas-list/src/index.ts
+++ packages/ideas-list/src/index.ts
@@ -1,5 +1,10 @@
 import { readFileSync } from "fs";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
 
 const ideas = readFileSync("/Users/taylormitchell/code/home/data/notes/ideas-list.md", "utf-8");
 
-console.log(ideas);
+// Parse markdown into AST
+const ast = unified().use(remarkParse).processSync(ideas).tree;
+
+console.log(JSON.stringify(ast, null, 2));
