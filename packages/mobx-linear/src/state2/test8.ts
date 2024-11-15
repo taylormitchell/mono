68db2d35fb48a1acb0ac29fa01b5e7cbe943a09e
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-14 21:42:02 -0500

    save

--- packages/mobx-linear/src/state2/test8.ts
+++ packages/mobx-linear/src/state2/test8.ts
@@ -334,21 +334,23 @@ class Backlinks<T extends Model> implements Iterable<T> {
   unsubscribe: (() => void) | null = null;
 
   constructor(private owner: Model, link: { from: ModelName; key: string }) {
-    this.unsubscribe = subscribe((event) => {
-      if (event.model === link.from) {
-        if (event.operation === "delete" && this.map.has(event.id)) {
-          this.map.delete(event.id);
-        } else if (event.operation === "update" && event.propKey === link.key) {
-          if (event.oldValue === this.owner.id) {
-            this.map.delete(event.id);
+    this.unsubscribe = _store
+      ? _store.subscribe((event) => {
+          if (event.model === link.from) {
+            if (event.operation === "delete" && this.map.has(event.id)) {
+              this.map.delete(event.id);
+            } else if (event.operation === "update" && event.propKey === link.key) {
+              if (event.oldValue === this.owner.id) {
+                this.map.delete(event.id);
+              }
+              if (event.newValue === this.owner.id) {
+                const model = getModel(link.from, event.id);
+                if (model) this.map.set(event.id, model);
+              }
+            }
           }
-          if (event.newValue === this.owner.id) {
-            const model = getModel(link.from, event.id);
-            if (model) this.map.set(event.id, model);
-          }
-        }
-      }
-    });
+        })
+      : null;
   }
 
   [Symbol.iterator](): Iterator<T> {
