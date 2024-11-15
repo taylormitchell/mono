e3b7cc1b8744a57ebb1946c4389eadd681dd0e80
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-14 22:58:01 -0500

    save

--- packages/mobx-linear/src/state2/test8.ts
+++ packages/mobx-linear/src/state2/test8.ts
@@ -428,22 +428,15 @@ class Relation implements IModel {
 }
 
 // Load initial data
-const store = _store;
-const issue1 = store.setIssue({ id: "i1", title: "Issue 1", projectId: "p1" });
-const issue2 = store.setIssue({ id: "i2", title: "Issue 2", projectId: "p1" });
-const project1 = store.setProject({ id: "p1", title: "Project 1" });
-const project2 = store.setProject({ id: "p2", title: "Project 2" });
+const issue1 = new Issue({ id: "i1", title: "Issue 1", projectId: "p1" });
+const issue2 = new Issue({ id: "i2", title: "Issue 2", projectId: "p1" });
+const project1 = new Project({ id: "p1", title: "Project 1" });
+const project2 = new Project({ id: "p2", title: "Project 2" });
 
 // Make changes
 runInAction(() => {
   project1.title = "Updated Title";
-  project1.issues.delete("i1");
-});
-
-const relation: Relation | null = null;
-runInAction(() => {
-  issue2.project = project2;
-  // relation = store.setRelation({ id: "r1", from: issue1, to: issue2 });
+  issue1.project = project2;
 });
 
 console.log(project1.issues);
