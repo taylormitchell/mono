7d8e0603452141ff0eedd1352d49574882f42913
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-15 00:17:01 -0500

    save

--- packages/mobx-linear/src/state2/test8.ts
+++ packages/mobx-linear/src/state2/test8.ts
@@ -254,14 +254,13 @@ const Property = (serializedName?: string) => {
   };
 };
 
-const ForeignKey = (referencedModelName: ModelName, serializedKey?: string) => {
+const ForeignKey = (serializedKey: string, referencedModelName: ModelName) => {
   return <T extends IModel>(target: any, context: ClassAccessorDecoratorContext) => {
     const observableResult = observable(target, context);
     if (!observableResult) {
       throw new Error("Failed to decorate property");
     }
     const modelKey = String(context.name);
-    serializedKey = serializedKey ?? modelKey;
 
     return {
       get(this: T) {
@@ -415,7 +414,7 @@ class Issue implements IModel {
   @Property()
   accessor title = "";
 
-  @ForeignKey("project", "projectId")
+  @ForeignKey("projectId", "project")
   accessor project: Project | null = null;
 
   relationsFrom = new Backlinks<Relation>(this, { from: "relation", key: "from" });
@@ -448,10 +447,10 @@ class Relation implements IModel {
 
   placeholder = false;
 
-  @ForeignKey("issue", "fromId")
+  @ForeignKey("fromId", "issue")
   accessor from: Issue | null = null;
 
-  @ForeignKey("issue", "toId")
+  @ForeignKey("toId", "issue")
   accessor to: Issue | null = null;
 }
 
