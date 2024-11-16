66fb670c38180fcb56ab6b6f4ded881f0d0b73f8
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-16 15:38:01 -0500

    save

--- packages/mobx-linear/src/state2/test8.ts
+++ packages/mobx-linear/src/state2/test8.ts
@@ -306,7 +306,6 @@ const ForeignKey = (serializedKey: string, referencedModelName: ModelName) => {
 
 const Model = (name: ModelName) => {
   return (value: any, { kind }: ClassDecoratorContext) => {
-    console.log("Model", name, kind);
     if (kind === "class") {
       function createInstance(props: any) {
         const inst = store.getModel(name, props.id) ?? new value(props);
@@ -348,50 +347,6 @@ const Model = (name: ModelName) => {
       }
       store.models[name].create = createInstance;
       return value;
-
-      return function (props: any) {
-        const inst = store.getModel(name, props.id) ?? new value(props);
-        // Resolve foreign keys to instances
-        Object.entries(store.models[name].foreignKeys).forEach(
-          ([modelKey, { referencedModelName, serializedKey: serializedKey }]) => {
-            if (props[serializedKey]) {
-              const referencedId = props[serializedKey];
-              let referencedInst: IModel | null = null;
-              if (referencedId) {
-                const inst = store.getModel(referencedModelName, referencedId);
-                if (inst) {
-                  referencedInst = inst;
-                } else {
-                  if (referencedModelName === "project") {
-                    referencedInst = new Project({ id: referencedId, placeholder: true });
-                  } else if (referencedModelName === "issue") {
-                    referencedInst = new Issue({ id: referencedId, placeholder: true });
-                  } else if (referencedModelName === "relation") {
-                    referencedInst = new Relation({ id: referencedId, placeholder: true });
-                  }
-                  // referencedInst = new store.models[referencedModelName].class({
-                  //   id: referencedId,
-                  // });
-                  // // TODO For some reason the above call doesn't trigger the Model decorator
-                  // // so we have to set placeholder manually.
-                  // referencedInst.placeholder = true;
-                }
-              }
-              inst[modelKey] = referencedInst;
-            }
-          }
-        );
-        // Set properties
-        Object.entries(store.models[name].properties).forEach(([modelKey, serializedKey]) => {
-          if (props[serializedKey]) {
-            inst[modelKey] = props[serializedKey];
-          }
-        });
-        inst.placeholder = props.placeholder ?? false;
-        store.models[name].instances.set(inst.id, inst);
-        store.emitEvent({ operation: "create", model: name, id: inst.id, props });
-        return inst;
-      };
     }
     return value;
   };
