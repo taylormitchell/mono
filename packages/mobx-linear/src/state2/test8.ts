94fa7f964e4ed30f45891c7b5191541715148d52
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-17 10:25:01 -0500

    save

--- packages/mobx-linear/src/state2/test8.ts
+++ packages/mobx-linear/src/state2/test8.ts
@@ -455,6 +455,7 @@ class Issue extends BaseModel {
   @ForeignKey("projectId", "project")
   accessor project: Project | null = null;
 
+  // @Backlinks("relation.from") TODO: Maybe this?
   @Backlinks({ from: "relation", key: "from" })
   readonly relationsFrom = new Set<Relation>();
 
