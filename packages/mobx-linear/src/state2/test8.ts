e967cb9b3490af971922741b379ecb191bfc57a0
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-16 23:41:01 -0500

    save

--- packages/mobx-linear/src/state2/test8.ts
+++ packages/mobx-linear/src/state2/test8.ts
@@ -336,45 +336,19 @@ const Model = (name: ModelName) => {
                 }
               }
             }
-            }
             if (event.operation === "create") {
-              instances.get(event.id)?.[backlinksKey]?.add(event.id);
+              const model = instances.get(event.id);
+              if (model) {
+                const referencedId = model[fromKey];
+                const referencedModel = store.models[referencedModelName].get(referencedId);
+                if (referencedModel) {
+                  referencedModel[backlinksKey]?.add(event.id);
+                }
+              }
             }
           }
         }
       );
-
-      if (event.model === name) {
-        if (event.operation === "delete") {
-          Object.values(getModelMetadata(cls).backlinks).forEach(
-            ({ fromModel, fromKey, backlinksKey }) => {
-              instances.get(event.id)?.[backlinksKey]?.delete(event.id);
-            }
-          );
-          instances.delete(event.id);
-          return;
-        }
-        const modelReferencingOwner = store.models[link.from].get(event.id);
-        if (!modelReferencingOwner) {
-          console.warn(`Received event for unknown model ${link.from} with id ${event.id}`);
-          return;
-        }
-        if (event.operation === "create") {
-          this.map.set(event.id, modelReferencingOwner);
-          return;
-        }
-        const serializedForeignKey = modelReferencingOwner
-          ? getModelMetadata(modelReferencingOwner.constructor).foreignKeys[link.key].serializedKey
-          : null;
-        if (event.operation === "update" && event.propKey === serializedForeignKey) {
-          if (event.oldValue === this.owner.id) {
-            this.map.delete(event.id);
-          }
-          if (event.newValue === this.owner.id) {
-            this.map.set(event.id, modelReferencingOwner);
-          }
-        }
-      }
     });
     store.models[name] = {
       // TODO: Should somehow make it more obvious that this is creating a new instance
