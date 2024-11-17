79390b5838baf89d7b86fde35f3449fe08f1dcf7
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-16 23:16:01 -0500

    save

--- packages/mobx-linear/src/state2/test8.ts
+++ packages/mobx-linear/src/state2/test8.ts
@@ -432,34 +432,34 @@ class Backlinks<T extends BaseModel> implements Iterable<T> {
 
 const BacklinkDecorator = (link: { from: ModelName; key: string }) => {
   return (target: any, context: ClassFieldDecoratorContext) => {
-    const unsubscribe = store.subscribe((event) => {
-      if (event.model === link.from) {
-        if (event.operation === "delete" && this.has(event.id)) {
-          super.delete(event.id);
-          return;
-        }
-        const modelReferencingOwner = store.models[link.from].get(event.id);
-        if (!modelReferencingOwner) {
-          console.warn(`Received event for unknown model ${link.from} with id ${event.id}`);
-          return;
-        }
-        if (event.operation === "create") {
-          super.add(modelReferencingOwner);
-          return;
-        }
-        const serializedForeignKey = modelReferencingOwner
-          ? getModelMetadata(modelReferencingOwner.constructor).foreignKeys[link.key].serializedKey
-          : null;
-        if (event.operation === "update" && event.propKey === serializedForeignKey) {
-          if (event.oldValue === this.owner.id) {
-            super.delete(modelReferencingOwner);
-          }
-          if (event.newValue === this.owner.id) {
-            super.add(modelReferencingOwner);
-          }
-        }
-      }
-    });
+    // const unsubscribe = store.subscribe((event) => {
+    //   if (event.model === link.from) {
+    //     if (event.operation === "delete" && this.has(event.id)) {
+    //       super.delete(event.id);
+    //       return;
+    //     }
+    //     const modelReferencingOwner = store.models[link.from].get(event.id);
+    //     if (!modelReferencingOwner) {
+    //       console.warn(`Received event for unknown model ${link.from} with id ${event.id}`);
+    //       return;
+    //     }
+    //     if (event.operation === "create") {
+    //       super.add(modelReferencingOwner);
+    //       return;
+    //     }
+    //     const serializedForeignKey = modelReferencingOwner
+    //       ? getModelMetadata(modelReferencingOwner.constructor).foreignKeys[link.key].serializedKey
+    //       : null;
+    //     if (event.operation === "update" && event.propKey === serializedForeignKey) {
+    //       if (event.oldValue === this.owner.id) {
+    //         super.delete(modelReferencingOwner);
+    //       }
+    //       if (event.newValue === this.owner.id) {
+    //         super.add(modelReferencingOwner);
+    //       }
+    //     }
+    //   }
+    // });
     context.addInitializer(() => {
       console.log("initializer");
     });
