78b31e25ceeba0745476c101a45e7a7d8d175c3a
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-22 23:55:01 -0500

    save

--- packages/mobx-linear/src/lib/store.ts
+++ packages/mobx-linear/src/lib/store.ts
@@ -315,6 +315,7 @@ export class Store<TModels extends ModelRecord> {
 
   emit(event: StoreEvent) {
     if (!this.emittingEnabled) return;
+    this.redoStack = []; // TODO is this the right place?
     this.pendingChanges.push(event);
     this.lastChangeTimestamp.set(Date.now());
     this.eventSubscribers.forEach((subscriber) => subscriber(event));
