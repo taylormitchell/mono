b650000d4d3c3d99bad27f554cb437484d1e2c70
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-22 22:10:01 -0500

    save

--- packages/mobx-linear/babel.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"],
-  plugins: [
-    // Enable legacy decorator syntax
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    // Enable class properties with loose mode for compatibility
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-  ],
-};
