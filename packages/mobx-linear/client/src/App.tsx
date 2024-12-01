7b262a0830ccfc5097169fb43658533516aeb78b
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-11-30 19:51:22 -0500

    save

--- packages/mobx-linear/client/src/App.tsx
+++ packages/mobx-linear/client/src/App.tsx
@@ -4,9 +4,9 @@ import styles from "./App.module.css";
 import { createStore, StoreContext } from "./lib/models";
 
 const store = createStore();
-store.create("issue", { title: "1" });
-store.create("issue", { title: "2" });
-store.create("issue", { title: "3" });
+store.create("issue", { title: "1", createdAt: 1 });
+store.create("issue", { title: "2", createdAt: 2 });
+store.create("issue", { title: "3", createdAt: 3 });
 
 const App = observer(() => {
   return (
