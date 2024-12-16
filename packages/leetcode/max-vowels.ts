9e54c0aabf5ef76712f3cc330b9b79e2a7670736
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-12-16 07:50:01 -0500

    save

--- packages/leetcode/max-vowels.ts
+++ packages/leetcode/max-vowels.ts
@@ -16,6 +16,8 @@
  * (this only does one less vowel check. we _could_ keep track of the each index is vowel or not, then we only need to check a boolean instead of a vowel array check)
  */
 
+const vowels = ["a", "e", "i", "o", "u"];
+
 function main(s: string, k: number): number {
   if (s.length === 0) {
     return 0;
@@ -24,5 +26,29 @@ function main(s: string, k: number): number {
   let currentVowels = 0;
   let start = 0;
   let end = Math.min(s.length - 1, k);
-  while (end < s.length) {}
+
+  // Count next vowel
+  while (end < s.length) {
+    if (start === 0) {
+      for (let i = start; i <= end; i++) {
+        currentVowels += vowels.includes(s[i]) ? 1 : 0;
+      }
+    } else {
+      if (vowels.includes[s[start - 1]]) {
+        currentVowels--;
+      }
+      if (vowels.includes[s[end]]) {
+        currentVowels++;
+      }
+    }
+    maxVowels = Math.max(maxVowels, currentVowels);
+    start++;
+    end++;
+  }
+
+  return maxVowels;
+}
+
+function test() {
+  main("abciiidef", 3);
 }
