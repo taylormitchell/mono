002ae29413a858eed7f2b2db7427a361517cf0e6
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-12-22 18:30:01 -0500

    save

--- packages/mobx-linear/client/src/lib/models.ts
+++ packages/mobx-linear/client/src/lib/models.ts
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import { property, backlinks, link, BaseModel, Store, updatedAt } from "./store";
-import { action, autorun, computed, toJS } from "mobx";
+import { action, computed } from "mobx";
 import { createPosition } from "./position";
 
 // TODO can probably define the store interface at the top and then
@@ -93,9 +93,6 @@ class IssueView extends BaseModel {
 
   constructor(props: { id?: string; placeholder?: boolean } = {}) {
     super(props);
-    autorun(() => {
-      console.log("issueViewPositions", toJS(this.issueViewPositions));
-    });
   }
 
   @computed
