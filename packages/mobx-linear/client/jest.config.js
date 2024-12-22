3f72d4aa9999c15cfee5af82259d985075bd0fb6
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-12-22 15:55:02 -0500

    save

--- packages/mobx-linear/client/jest.config.js
+++ packages/mobx-linear/client/jest.config.js
@@ -4,6 +4,8 @@ export default {
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
   },
+  transformIgnorePatterns: ["node_modules/(?!(fractional-indexing)/)"],
+  extensionsToTreatAsEsm: [".ts", ".tsx", ".jsx"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
