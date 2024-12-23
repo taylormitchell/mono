a37c7c0067151363833b321414293908c77d2981
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-12-22 22:45:01 -0500

    save

--- packages/mobx-linear/client/src/App.tsx
+++ packages/mobx-linear/client/src/App.tsx
@@ -2,14 +2,13 @@ import { observer } from "mobx-react-lite";
 import { IssueList } from "./components/IssueList";
 import styles from "./App.module.css";
 import { createStore, StoreContext } from "./lib/models";
+import issues from "../public/react-issues.json";
 
 const store = createStore();
-// store.create("issue", { title: "1", createdAt: 3 });
-// store.create("issue", { title: "2", createdAt: 2 });
-// store.create("issue", { title: "3", createdAt: 1 });
-store.create("issue", { title: "a1", createdAt: 3 });
-store.create("issue", { title: "a2", createdAt: 2 });
-store.create("issue", { title: "b1", createdAt: 1 });
+// store.create("issue", { title: "a1", createdAt: 3 });
+// store.create("issue", { title: "a2", createdAt: 2 });
+// store.create("issue", { title: "b1", createdAt: 1 });
+issues.forEach((issue) => store.create("issue", issue));
 
 const App = observer(() => {
   return (
