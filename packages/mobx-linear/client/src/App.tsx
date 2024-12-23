9952fac6aa0faeea52208e3f9efc3954abdb2743
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2024-12-22 22:50:03 -0500

    save

--- packages/mobx-linear/client/src/App.tsx
+++ packages/mobx-linear/client/src/App.tsx
@@ -8,7 +8,9 @@ const store = createStore();
 // store.create("issue", { title: "a1", createdAt: 3 });
 // store.create("issue", { title: "a2", createdAt: 2 });
 // store.create("issue", { title: "b1", createdAt: 1 });
-issues.forEach((issue) => store.create("issue", issue));
+issues.forEach((issue) =>
+  store.create("issue", { title: issue.title, createdAt: new Date(issue.created_at).getTime() })
+);
 
 const App = observer(() => {
   return (
