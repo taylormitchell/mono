51be21387a12c7867e06f6688f0e7da47ce846fa
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-01-10 21:20:02 -0500

    save

--- packages/interview-prep-2024/nested-checkbox/src/App.tsx
+++ packages/interview-prep-2024/nested-checkbox/src/App.tsx
@@ -39,29 +39,56 @@ const defaultTree: Tree = {
   ],
 };
 
+function toggle(nodes: Node[], index: number[], depth: number = 0): Node[] {
+  return nodes.map((node, i) => {
+    if (i === index[depth]) {
+      if (depth === index.length - 1) {
+        return { ...node, isChecked: !node.isChecked };
+      } else {
+        return { ...node, children: toggle(node.children, index, depth + 1) };
+      }
+    }
+    return node;
+  });
+}
+
 function App() {
   const [tree, setTree] = useState(defaultTree);
+  const toggleChecked = (index: number[]) => {
+    setTree((tree) => ({
+      ...tree,
+      children: toggle(tree.children, index),
+    }));
+  };
 
   return (
     <div>
       <h1>Nested Checkbox</h1>
-      {tree.children.map((child) => (
-        <Tree node={child} />
+      {tree.children.map((child, index) => (
+        <Tree node={child} index={[index]} toggleChecked={toggleChecked} />
       ))}
     </div>
   );
 }
 
-function Tree({ node }: { node: Node }) {
+function Tree({
+  node,
+  index,
+  toggleChecked,
+}: {
+  node: Node;
+  index: number[];
+  toggleChecked: (index: number[]) => void;
+}) {
   return (
     <div>
       <div className="flex items-center gap-2">
-        <input type="checkbox" />
+        <input type="checkbox" checked={node.isChecked} onChange={() => toggleChecked(index)} />
         {node.label}
       </div>
       <div className="pl-4">
-        {node.children.map((child) => (
-          <Tree node={child} />
+        {node.children.map((child, i) => (
+          <Tree node={child} index={[...index, i]} toggleChecked={toggleChecked} />
         ))}
       </div>
     </div>
