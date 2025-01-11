5384ab5c827be501a649afbcfdd421c055b2aad0
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-01-10 21:30:01 -0500

    save

--- packages/interview-prep-2024/nested-checkbox/src/tree.ts
+++ packages/interview-prep-2024/nested-checkbox/src/tree.ts
@@ -4,12 +4,15 @@ type Node = {
   children: Node[];
 };
 
-export function toggle(node: Node, index: number[], depth: number = 0): Node {
-  if (depth === index.length - 1) {
-    return { ...node, isChecked: !node.isChecked };
-  }
-  return {
-    ...node,
-    children: node.children.map((child) => toggle(child, index, depth + 1)),
-  };
+function toggle(nodes: Node[], index: number[], depth: number = 0): Node[] {
+  return nodes.map((node, i) => {
+    if (i === index[depth]) {
+      if (depth === index.length - 1) {
+        return { ...node, isChecked: !node.isChecked };
+      } else {
+        return { ...node, children: toggle(node.children, index, depth + 1) };
+      }
+    }
+    return node;
+  });
 }
