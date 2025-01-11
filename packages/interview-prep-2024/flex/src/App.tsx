2fa010be360c3c5caef233fdc0596b140661ead6
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-01-11 12:25:02 -0500

    save

--- packages/interview-prep-2024/flex/src/App.tsx
+++ packages/interview-prep-2024/flex/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const containerRef = useRef<HTMLDivElement>(null);
+  const defaultWidth = 600;
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <input
+        type="range"
+        min="100"
+        max="600"
+        defaultValue={defaultWidth}
+        onChange={(e) => {
+          const width = e.target.value;
+          if (containerRef.current) {
+            containerRef.current.style.width = `${width}px`;
+          }
+        }}
+      />
+      <div ref={containerRef} style={{ width: `${defaultWidth}px` }}>
+        <form>
+          <input className="bg-gray-200" type="text" placeholder="Name" />
+          <input className="bg-gray-200" type="email" placeholder="Email Address" />
+          <input className="bg-blue-200" type="submit" value="Subscribe" />
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
