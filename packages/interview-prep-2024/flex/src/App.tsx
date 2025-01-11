92fa005ac5069a527bdc3af3ab17d42941ca3416
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-01-11 12:35:01 -0500

    save

--- packages/interview-prep-2024/flex/src/App.tsx
+++ packages/interview-prep-2024/flex/src/App.tsx
@@ -19,10 +19,10 @@ function App() {
         }}
       />
       <div className="p-4 border" ref={containerRef} style={{ width: `${defaultWidth}px` }}>
-        <form>
-          <input className="bg-gray-200" type="text" placeholder="Name" />
-          <input className="bg-gray-200" type="email" placeholder="Email Address" />
-          <input className="bg-blue-200" type="submit" value="Subscribe" />
+        <form className="flex">
+          <input className="bg-gray-200 min-w-0" type="text" placeholder="Name" />
+          <input className="bg-gray-200 min-w-0" type="text" placeholder="Name" />
+          <input className="bg-blue-200 min-w-0" type="submit" value="Subscribe" />
         </form>
       </div>
     </div>
