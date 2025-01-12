b9d96fcc1d33f756f8d3f242507877f7d3237978
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-01-12 18:00:02 -0500

    save

--- packages/interviews/shopify-2025-01-12/src/App.tsx
+++ packages/interviews/shopify-2025-01-12/src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+
+function test() {
+  console.log("test");
+}
+
 function App() {
+  useEffect(() => {
+    test();
+  }, []);
+
   return (
     <div>
       <h1>Shopify interview</h1>
+      <p>
+        This is a test for the Shopify interview. It is a simple React app that displays a list of
+        products.
+      </p>
     </div>
   );
 }
