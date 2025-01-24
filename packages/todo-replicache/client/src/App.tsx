8808e5f40b1118747f5b44097d09c0be571caf83
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-01-24 14:35:09 -0700

    save

--- packages/todo-replicache/client/src/App.tsx
+++ packages/todo-replicache/client/src/App.tsx
@@ -5,6 +5,7 @@ import { createStore, genId, Store } from "./store";
 import { Todo } from "../../shared/types";
 
 function debounce(fn: (...args: any[]) => void, ms: number) {
+  console.log("creating debounce");
   let timeout: ReturnType<typeof setTimeout> = 0;
   const debouncedFn = (...args: any[]) => {
     clearTimeout(timeout);
@@ -18,6 +19,7 @@ function useDebounce(fn: (...args: any[]) => void, deps: any[], ms: number) {
   const debouncedFn = useMemo(() => debounce(fn, ms), [deps, ms]);
   useEffect(() => {
     return () => {
+      console.log("cancelling debounce");
       debouncedFn.cancel();
     };
   }, [debouncedFn]);
@@ -134,36 +136,13 @@ function App() {
         {todos
           .sort((a, b) => b.createdAt.localeCompare(a.createdAt)) // Reverse chronological order
           .map((todo) => (
-            <div
+            <TodoItem
               key={todo.id}
-              className="flex items-center justify-between p-3 bg-white rounded shadow"
-            >
-              {editingId === todo.id ? (
-                <input
-                  type="text"
-                  value={todo.content}
-                  onChange={(e) => store.todos.update(todo.id, { content: e.target.value })}
-                  onBlur={() => setEditingId(null)}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      setEditingId(null);
-                    }
-                  }}
-                  className="flex-1 px-2 py-1 border rounded"
-                  autoFocus
-                />
-              ) : (
-                <span className="text-gray-800 flex-1" onClick={() => setEditingId(todo.id)}>
-                  {todo.content}
-                </span>
-              )}
-              <button
-                onClick={() => store.todos.delete(todo.id)}
-                className="ml-2 px-2 py-1 text-red-500 hover:bg-red-100 rounded"
-              >
-                x
-              </button>
-            </div>
+              todo={todo}
+              editingId={editingId}
+              setEditingId={setEditingId}
+              store={store}
+            />
           ))}
       </div>
     </div>
@@ -185,6 +164,7 @@ function TodoItem({
 
   const debouncedUpdate = useDebounce(
     (id: string, content: string) => {
+      console.log("debouncing update");
       store.todos.update(id, { content });
     },
     [store],
