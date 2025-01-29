5397f5aed2a1fc71d0e0221d434fd4f3c29bcfea
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-01-29 08:15:01 -0500

    save

--- packages/todo-replicache/client/src/App.tsx
+++ packages/todo-replicache/client/src/App.tsx
@@ -4,7 +4,6 @@ import { createStore, genId, Store } from "./store";
 import { Todo, View } from "../../shared/types";
 import { isHotkey } from "is-hotkey";
 import { useDebounce } from "./utils";
-import { DndContext, DragEndEvent, useDraggable, useDroppable } from "@dnd-kit/core";
 
 declare global {
   interface Window {
@@ -199,19 +198,6 @@ function TodoView({ view }: { view: View }) {
 
   console.log({ filteredTodos, view });
 
-  function handleDragEnd(event: DragEndEvent) {
-    const { active, over } = event;
-    if (!over) return;
-
-    const fromIndex = filteredTodos.findIndex((todo) => todo.id === active.id);
-    const toIndex = filteredTodos.findIndex((todo) => todo.id === over.id);
-
-    if (fromIndex !== toIndex) {
-      const newPositions = moveTo(filteredTodos, fromIndex, toIndex, view.positions);
-      store.views.update(view.id, { ...view, positions: newPositions });
-    }
-  }
-
   return (
     <div className="flex-1">
       <div className="rounded-md border border-[#30363d] bg-[#161b22] overflow-hidden">
@@ -225,49 +211,40 @@ function TodoView({ view }: { view: View }) {
           />
         </div>
 
-        <DndContext onDragEnd={handleDragEnd}>
-          <div className="divide-y divide-[#30363d]">
-            {filteredTodos.map((todo) => (
-              <DraggableTodo
-                key={todo.id}
-                todo={todo}
-                editingId={editingId}
-                setEditingId={setEditingId}
-              />
-            ))}
-          </div>
-        </DndContext>
-      </div>
-    </div>
-  );
-}
-
-function DraggableTodo({
-  todo,
-  editingId,
-  setEditingId,
-}: {
-  todo: Todo;
-  editingId: string | null;
-  setEditingId: (id: string | null) => void;
-}) {
-  const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
-    id: todo.id,
-  });
-  const { setNodeRef: setDroppableRef } = useDroppable({
-    id: todo.id,
-  });
-
-  const style = transform
-    ? {
-        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-      }
-    : undefined;
-
-  return (
-    <div ref={setDroppableRef} className={cn("relative", isDragging && "opacity-50")}>
-      <div ref={setNodeRef} style={style} {...attributes} {...listeners} className="touch-none">
-        <TodoItem todo={todo} editingId={editingId} setEditingId={setEditingId} />
+        <div className="divide-y divide-[#30363d]">
+          {filteredTodos.map((todo, i) => (
+            <div key={todo.id} className="flex flex-col">
+              <div className="flex items-center justify-between">
+                <TodoItem todo={todo} editingId={editingId} setEditingId={setEditingId} />
+                {view.sort.field === "position" && (
+                  <div className="flex items-center gap-2">
+                    <button
+                      onClick={() => {
+                        const newPositions = moveTo(filteredTodos, i, i - 1, view.positions);
+                        store.views.update(view.id, { ...view, positions: newPositions });
+                      }}
+                    >
+                      Move up
+                    </button>
+                    <button
+                      onClick={() => {
+                        const newPositions = moveTo(filteredTodos, i, i + 1, view.positions);
+                        store.views.update(view.id, { ...view, positions: newPositions });
+                      }}
+                    >
+                      Move down
+                    </button>
+                  </div>
+                )}
+              </div>
+              <div className="flex items-center">
+                <div>{todo.id}</div>
+                <div>{"->"}</div>
+                <div>{view.positions[todo.id] ?? "undefined"}</div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
