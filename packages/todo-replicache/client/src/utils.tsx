ea5d1c82878b029686bfc1b4635e160d8a755f97
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-01-30 08:56:27 -0500

    save

--- packages/todo-replicache/client/src/utils.tsx
+++ packages/todo-replicache/client/src/utils.tsx
@@ -2,12 +2,12 @@ import { useRef, useCallback, useEffect } from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function useDebounce(fn: (...args: any[]) => void, deps: any[], ms: number) {
-  const timeout = useRef<number>(0);
+  const timeout = useRef<ReturnType<typeof setTimeout> | null>(null);
 
   const debouncedFn = useCallback(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (...args: any[]) => {
-      clearTimeout(timeout.current);
+      if (timeout.current) clearTimeout(timeout.current);
       timeout.current = setTimeout(() => fn(...args), ms);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
