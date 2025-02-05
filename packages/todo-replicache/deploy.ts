7bd1acce1ba81e156b63a206bb44908c0ebe0b78
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-02-05 17:25:01 -0500

    save

--- packages/todo-replicache/deploy.ts
+++ packages/todo-replicache/deploy.ts
@@ -4,17 +4,15 @@ async function main() {
   await nginxManager.addApp("items", 3078);
   await nginxManager.push();
   const config = await nginxManager.loadConfig();
-  //   await $`ssh ${config.sshHost} '
-  //     cd ~/code/packages/todo-replicache &&
-  //     git pull &&
-  //     cd client &&
-  //     npm install &&
-  //     npm run build &&
-  //     cd ../server &&
-  //     npm install &&
-  //     npm run build &&
-  //     pm2 restart todo-replicache
-  //   '`;
+  await $`scp server/.env.production ${config.sshHost}:~/code/home/packages/todo-replicache/server/.env`;
+  await $`ssh ${config.sshHost} '
+      cd ~/code/home/packages/todo-replicache &&
+      git pull &&
+      cd client && npm install && npm run build &&
+      cd ../server && npm install && npm run build &&
+      echo PORT=${config.apps.items.port} >> .env &&
+      pm2 restart todo-replicache
+    '`;
 }
 
 main();
--- packages/todo-replicache/server/src/index.ts
+++ packages/todo-replicache/server/src/index.ts
@@ -4,7 +4,9 @@ import { handlePush, handlePull } from "./lib/replicache";
 import { replicacheClientTable, replicacheServerTable, itemTable } from "./lib/db/schema";
 import cors from "cors";
 import path from "path";
-import "dotenv/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3078;
