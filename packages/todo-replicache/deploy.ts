466fa77190f783b9a45ef5e62512ab9630cb5ede
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-02-05 17:50:02 -0500

    save

--- packages/todo-replicache/deploy.ts
+++ packages/todo-replicache/deploy.ts
@@ -1,15 +1,14 @@
-import * as nginxManager from "../infra/ec2/nginx-manager";
+import * as ec2 from "../infra/ec2/manager";
 import { $ } from "bun";
 
 async function main() {
-  await nginxManager.addApp("items", 3078);
-  await nginxManager.push();
-  const config = await nginxManager.loadConfig();
+  await ec2.addApp("items", 3078);
+  await ec2.push();
+  const config = await ec2.loadConfig();
   await $`scp server/.env.production ${config.sshHost}:~/code/home/packages/todo-replicache/server/.env`;
   await $`scp client/.env.production ${config.sshHost}:~/code/home/packages/todo-replicache/client/.env`;
   await $`ssh ${config.sshHost} '
-        cd ~/code/home/packages/todo-replicache &&
-        git pull &&
+        cd ~/code/home/packages/todo-replicache && git pull && 
         cd client && npm install && npm run build &&
         cd ../server && npm install && npm run build &&
         echo PORT=${config.apps.items.port} >> .env &&
