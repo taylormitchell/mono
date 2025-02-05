d5ac0871a994250e0d3a19c82cdb7032a08a5f31
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-02-05 17:40:02 -0500

    save

--- packages/todo-replicache/deploy.ts
+++ packages/todo-replicache/deploy.ts
@@ -6,14 +6,15 @@ async function main() {
   await nginxManager.push();
   const config = await nginxManager.loadConfig();
   await $`scp server/.env.production ${config.sshHost}:~/code/home/packages/todo-replicache/server/.env`;
+  await $`scp client/.env.production ${config.sshHost}:~/code/home/packages/todo-replicache/client/.env`;
   await $`ssh ${config.sshHost} '
-      cd ~/code/home/packages/todo-replicache &&
-      git pull &&
-      cd client && npm install && npm run build &&
-      cd ../server && npm install && npm run build &&
-      echo PORT=${config.apps.items.port} >> .env &&
-      pm2 delete items || true && pm2 start 'bun start' --name items
-    '`;
+        cd ~/code/home/packages/todo-replicache &&
+        git pull &&
+        cd client && npm install && npm run build &&
+        cd ../server && npm install && npm run build &&
+        echo PORT=${config.apps.items.port} >> .env &&
+        pm2 delete items || true && pm2 start "bun start" --name items
+      '`;
 }
 
 main();
