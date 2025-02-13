d9bfb5a05fc9964d23a7d876a0697c1a82f135ae
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-02-12 22:10:01 -0500

    save

--- packages/log2/client/src/App.tsx
+++ packages/log2/client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { createStore, Store } from "./store";
-import { StoreContext, useStore } from "./hooks/store";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { StoreContext } from "./hooks/store";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { isHotkey } from "is-hotkey";
 import { StandardView } from "./pages/StandardView";
 
@@ -22,9 +22,22 @@ function StoreProvider({ children }: { children: React.ReactNode }) {
     s.rep.pull();
     window.store = s;
     setStore({ isLoading: false, store: s });
+    const handleKeyPress = async (e: KeyboardEvent) => {
+      if (isHotkey("cmd+z", e)) {
+        e.preventDefault();
+        e.stopPropagation();
+        s.undo();
+      }
+      if (isHotkey("cmd+shift+z", e)) {
+        e.preventDefault();
+        e.stopPropagation();
+        s.redo();
+      }
+    };
+    window.addEventListener("keydown", handleKeyPress);
     return () => {
-      // s.destroy();
-      window.store = null;
+      s.destroy();
+      window.removeEventListener("keydown", handleKeyPress);
     };
   }, []);
 
@@ -32,45 +45,15 @@ function StoreProvider({ children }: { children: React.ReactNode }) {
   return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>;
 }
 
-function Layout({ children }: { children: React.ReactNode }) {
-  const store = useStore();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const handleKeyPress = async (e: KeyboardEvent) => {
-      if (isHotkey("cmd+z", e)) {
-        e.preventDefault();
-        e.stopPropagation();
-        store.undo();
-      }
-      if (isHotkey("cmd+shift+z", e)) {
-        e.preventDefault();
-        e.stopPropagation();
-        store.redo();
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyPress);
-    return () => window.removeEventListener("keydown", handleKeyPress);
-  }, [store]);
-
-  return (
-    <div className="h-full w-full bg-[var(--bg-primary)] flex relative overflow-hidden">
-      <div className="h-full w-full">{children}</div>
-    </div>
-  );
-}
-
 function App() {
   return (
     <StoreProvider>
       <Router>
-        <Layout>
+        <div className="h-full w-full bg-[var(--bg-primary)] flex relative overflow-hidden">
           <Routes>
             <Route path="/" element={<StandardView />} />
           </Routes>
-        </Layout>
+        </div>
       </Router>
     </StoreProvider>
   );
