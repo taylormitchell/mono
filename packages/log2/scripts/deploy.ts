1240017359d88a972966495be6bc65a80428fc55
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-02-13 21:50:01 -0500

    save

--- packages/log2/scripts/deploy.ts
+++ packages/log2/scripts/deploy.ts
@@ -19,7 +19,7 @@ async function main() {
         cd client && npm install && npm run build &&
         cd ../server && npm install &&
         bun run db:up &&
-        echo PORT=${config.apps.log.port} >> .env &&
+        echo "\nPORT=${config.apps.log.port}" >> .env &&
         pm2 delete log || true && pm2 start "bun start" --name log
       '`;
 }
