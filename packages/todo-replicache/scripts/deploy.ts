ae7eb0f466810d5181ed10a03d5456c6d0a8f47f
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-02-17 10:48:24 -0500

    save

--- packages/todo-replicache/scripts/deploy.ts
+++ packages/todo-replicache/scripts/deploy.ts
@@ -2,22 +2,24 @@ import * as ec2 from "../../infra/ec2/manager";
 import { $ } from "bun";
 
 async function main() {
-  try {
-    await $`cd client && npm run typecheck`;
-  } catch (e) {
-    console.error("Typecheck failed - aborting deploy");
-    process.exit(1);
-  }
+  await $`cd client && npm run build`;
   const config = await ec2.addApp("items", 3078);
   await ec2.pushNginxConf();
   await ec2.pullRepo();
   const appDir = config.repoDir + "/packages/todo-replicache";
+
+  // Copy build to server
+  await $`ssh ${config.sshHost} 'rm -rf ${appDir}/client/dist'`;
+  await $`scp -r client/dist ${config.sshHost}:${appDir}/client/`;
+
+  // Copy envs to server
   await $`scp server/.env.production ${config.sshHost}:${appDir}/server/.env`;
   await $`scp client/.env.production ${config.sshHost}:${appDir}/client/.env`;
+
+  // Install deps, build, and start
   await $`ssh ${config.sshHost} '
-        cd ${appDir} &&
-        cd client && npm install && npm run build &&
-        cd ../server && npm install &&
+        cd ${appDir}/server &&
+        npm install &&
         bun run db:up &&
         echo .env && echo PORT=${config.apps.items.port} >> .env &&
         pm2 delete items || true && pm2 start "bun start" --name items
