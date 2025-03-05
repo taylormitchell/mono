8280d12e4a3ec898e200e250337cf31ab2c5613c
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2025-03-04 19:27:58 -0500

    save

--- packages/log2/server/src/lib/load-env.ts
+++ packages/log2/server/src/lib/load-env.ts
@@ -1,7 +1,4 @@
 import dotenv from "dotenv";
-// Load the .env from the project root
-dotenv.config({ path: "../.env" });
-// Load the .env.production or .env.development file
 dotenv.config({
   path: "../" + (process.env.NODE_ENV === "production" ? ".env.production" : ".env.development"),
 });
